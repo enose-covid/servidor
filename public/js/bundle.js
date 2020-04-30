4c5b1319d0993669c6e503a93d28fb76448a0dbc
Juan Bta Talens <socjuan.jbt@gmail.com>
2020-04-30 19:35:37 +0200

    cambira el bundle-home.js

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -5,4 +5,5 @@ const MDCRipple = mdc.ripple.MDCRipple;
 const username = new MDCTextField(document.querySelector('.username'));
 const password = new MDCTextField(document.querySelector('.password'));
 
+alert("alerta");
 //new MDCRipple(document.querySelector('.next'));
